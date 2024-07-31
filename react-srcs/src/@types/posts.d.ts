f1761ec08b2b8dbbabb72b6c8677d492bcf2ce68
Kaio <kakacnmendes@gmail.com>
2024-07-31 11:40:57 -0300

    get request

---
+++ react-srcs/src/@types/posts.d.ts
@@ -0,0 +1,5 @@
+declare type Posts = {
+    id?: string;
+    title: string;
+    desc: string;
+}
--- react-srcs/src/routes/Home.tsx
+++ react-srcs/src/routes/Home.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-import { Link } from "react-router-dom";
+
 
 // import "./Home.css"
 
 const Home = () => {
+
+    // ! REQUISIÇÃO GET
     const [posts, setPosts] = useState([])
 
     const getPosts = async () => {
@@ -13,12 +15,20 @@ const Home = () => {
         
         try {
             
-            const response = await axios.get("")
+            // armazenar a resposta da API
+            const response = await axios.get("http://localhost:8000/todos");
+            
+            // armazenar apenas os dados retornados pela resposta
+            const data = response.data
 
+            console.log(data)
+
+            // atualizar os posts salvos nos dados
+            setPosts(data)
 
         } catch (error) {
-            
-        }
+            console.log(error);
+        };
     };
 
     useEffect( () => {
@@ -27,6 +37,23 @@ const Home = () => {
 
     }, []);
     // array vazio -> useEffect só executa uma vez, no loading da pág
+    // no loading da pag -> executa essa função para receber o JSON
+    // ! FIM REQUISIÇÃO GET
+
+    return (
+        <div>
+            <h1>Atividades</h1>
+            {posts.length === 0 ? (<p>Carregando ...</p>) : (
+                posts.map((post: Posts, index) => (
+                    <div className="post" key={index}>
+                        <h2>{post.title}</h2>
+                        <p>{post.desc}</p>
+                    </div>
+                ))
+            )}
+                
+        </div>
+    );
 };
 
 export default Home;
